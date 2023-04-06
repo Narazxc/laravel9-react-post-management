f58731197b0d78a950ffc1271f2ff22a86f7f163
Sitha Sovannara <sithasovannara51@gmail.com>
2023-04-06 18:18:11 +0700

    frontend: added new looks for table in Manage Category page and formatted date using date-fns

--- frontend/src/components/pages/Categories.jsx
+++ frontend/src/components/pages/Categories.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+// date fns
+import formantDistanceToNow from "date-fns/formatDistanceToNow";
 
 export default function Categories() {
   const [categories, setCategories] = useState([]);
@@ -26,25 +28,88 @@ export default function Categories() {
     }
   }, []);
   return (
-    <div>
-      <h2 className="text-6xl text-red-200 font-bold">Categories</h2>
-      <table className="table-auto">
-        <thead>
-          <tr>
-            <th>id</th>
-            <th>Categories</th>
-          </tr>
-        </thead>
-        <tbody>
-          {categories &&
-            categories.map((category) => (
-              <tr key={category.id}>
-                <td>{category.id}</td>
-                <td>{category.name}</td>
-              </tr>
-            ))}
-        </tbody>
-      </table>
-    </div>
+    <>
+      <div>
+        <h2 className="text-6xl text-red-200 font-bold">Categories</h2>
+        {/* <table className="table-auto">
+          <thead>
+            <tr>
+              <th>id</th>
+              <th>Categories</th>
+            </tr>
+          </thead>
+          <tbody>
+            {categories &&
+              categories.map((category) => (
+                <tr key={category.id}>
+                  <td>{category.id}</td>
+                  <td>{category.name}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table> */}
+      </div>
+      <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead class="text-md text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" class="px-6 py-3">
+                #
+              </th>
+              <th scope="col" class="px-6 py-3">
+                <div class="flex items-center">
+                  Category
+                  <a href="#"></a>
+                </div>
+              </th>
+              <th scope="col" class="px-6 py-3">
+                <div class="flex items-center">
+                  Created at
+                  <a href="#"></a>
+                </div>
+              </th>
+              <th scope="col" class="px-6 py-3">
+                <div class="flex items-center">
+                  Action
+                  <a href="#"></a>
+                </div>
+              </th>
+
+              {/* <th scope="col" class="px-6 py-3">
+                <span class="sr-only">Edit</span>
+              </th> */}
+            </tr>
+          </thead>
+          <tbody>
+            {categories &&
+              categories.map((category) => (
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                  <th
+                    scope="row"
+                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    {category.id}
+                  </th>
+                  <td class="px-6 py-4">{category.name}</td>
+                  <td class="px-6 py-4">
+                    {formantDistanceToNow(new Date(category.created_at), {
+                      addSuffix: true,
+                    })}
+                  </td>
+
+                  <td class="px-6 py-4">
+                    <a
+                      href="#"
+                      class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                    >
+                      Edit
+                    </a>
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
